349c31216a1b8d65b9546524ef8304a709aa5027
Onur Osman Güler <guleronurosman@gmail.com>
2020-07-17 18:45:02 +0300

    Wrap up getting started

---
+++ Messenger/Controllers/NewConversationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewConversationViewController.swift
+//  Messenger
+//
+//  Created by Onur Osman Güler on 17.07.2020.
+//  Copyright © 2020 Onur Osman Güler. All rights reserved.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Messenger/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewerViewController.swift
+//  Messenger
+//
+//  Created by Onur Osman Güler on 17.07.2020.
+//  Copyright © 2020 Onur Osman Güler. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
