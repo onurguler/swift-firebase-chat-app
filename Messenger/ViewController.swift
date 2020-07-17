341bef4c9186b43d21266c921566be10c0cb5514
Onur Osman Güler <guleronurosman@gmail.com>
2020-07-17 18:27:12 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Onur Osman Güler on 17.07.2020.
+//  Copyright © 2020 Onur Osman Güler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
