e4aeec201ec3d93d5b5a7717971205ee0378cb43
Onur Osman Güler <guleronurosman@gmail.com>
2020-07-19 04:10:39 +0300

    Add logout functionality

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -15,7 +15,7 @@ class ConversationsViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        // view.backgroundColor = .red
     }
 
     override func viewDidAppear(_ animated: Bool) {
